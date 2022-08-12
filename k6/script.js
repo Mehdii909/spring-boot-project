5ab90de9265fb18a4b133c6c531d29ad3df725a9
Mehdii909 <76838172+Mehdii909@users.noreply.github.com>
2022-08-12 21:55:44 +0100

    test commit

--- k6/script.js
+++ k6/script.js
@@ -6,7 +6,14 @@ const BASE_URL = "http://localhost:8080";
 // You might want to edit the value of this variable or remove calls to the sleep function on the script.
 const SLEEP_DURATION = 1;
 // Global variables should be initialized.
-
+export const options = {
+    duration: '0.5m',
+    vus: 20,
+    thresholds: {
+        http_req_failed: ['rate<0.01'], // http errors should be less than 1%
+        http_req_duration: ['p(95)<500'], // 95 percent of response times must be below 500ms
+    },
+};
 export default function() {
     group("/saveProduit test", () => {
 
