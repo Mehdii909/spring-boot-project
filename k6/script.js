b85c097a9125fa05659f9468465b6526cccbcd75
Mehdii909 <76838172+Mehdii909@users.noreply.github.com>
2022-08-12 22:06:06 +0100

    test commit

--- k6/script.js
+++ k6/script.js
@@ -1,7 +1,7 @@
 import http from "k6/http";
 import { group, check, sleep } from "k6";
 
-const BASE_URL = "http://localhost:8080";
+const BASE_URL = "mem";
 // Sleep duration between successive requests.
 // You might want to edit the value of this variable or remove calls to the sleep function on the script.
 const SLEEP_DURATION = 1;
