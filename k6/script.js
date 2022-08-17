9734a596940744705a29da25e1dc7bc26462bc29
Mehdii909 <76838172+Mehdii909@users.noreply.github.com>
2022-08-17 11:21:03 +0100

    local run project

--- k6/script.js
+++ k6/script.js
@@ -8,7 +8,7 @@ const SLEEP_DURATION = 1;
 // Global variables should be initialized.
 export const options = {
     duration: '0.5m',
-    vus: 20,
+    vus: 10,
     thresholds: {
         http_req_failed: ['rate<0.01'], // http errors should be less than 1%
         http_req_duration: ['p(95)<500'], // 95 percent of response times must be below 500ms
